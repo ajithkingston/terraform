04bf7030465008ac1ec7b76405397ae97d8fed0a
ajithkingston <110417922+ajithkingston@users.noreply.github.com>
2025-02-28 01:53:52 +0530

    try basic infra tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "TFSTATE-RG"
-    storage_account_name = "mytfstatestorageaccount"
+    storage_account_name = "mytfstatestorageacc"
     container_name       = "mytfstatestoragecontainer"
     key                  = "terraform.tfstate"
   }
--- main.tf
+++ main.tf
@@ -18,16 +18,124 @@ provider "azurerm" {
   }
 }
 
-# resource "azurerm_resource_group" "terraform-rg" {
-#   name     = "Terraform-RG"
-#   location = "southindia"
-#   tags = {
-#     owner      = "Ajith"
-#     env        = "dev"
-#     costcentre = "hands-on"
-#   }
+resource "azurerm_resource_group" "terraform-rg" {
+  name     = "Terraform-RG"
+  location = "southindia"
+  tags = {
+    owner      = "Ajith"
+    env        = "dev"
+    costcentre = "hands-on"
+  }
 
-# }
+}
+
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "southindia"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a network security group
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate the NSG with the subnet
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+# Create a public IP address
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Create a Linux virtual machine
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s" # Free tier eligible size
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub") # Replace with your public key path
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+# Output the public IP address of the VM
+output "public_ip_address" {
+  value = azurerm_public_ip.example.ip_address
+}
 
 # resource "azurerm_resource_group" "tfstate-rg" {
 #   name     = "TFSTATE-RG"
@@ -64,43 +172,43 @@ provider "azurerm" {
 
 # }
 
-resource "azurerm_resource_group" "JavaSpringSampleDeploy-RG" {
-  name     = "JavaSpringSampleDeploy-RG"
-  location = "East US"
+# resource "azurerm_resource_group" "JavaSpringSampleDeploy-RG" {
+#   name     = "JavaSpringSampleDeploy-RG"
+#   location = "East US"
 
-  tags = {
-    owner      = "Ajith"
-    env        = "dev"
-    costcentre = "hands-on"
-  }
-}
+#   tags = {
+#     owner      = "Ajith"
+#     env        = "dev"
+#     costcentre = "hands-on"
+#   }
+# }
 
-resource "azurerm_service_plan" "azurermserviceplan" {
-  name                = "JavaSpringSampleDeployServicePlan"
-  location            = "East US"
-  resource_group_name = "JavaSpringSampleDeploy-RG"
-  os_type             = "Linux"
-  sku_name            = "F1"
+# resource "azurerm_service_plan" "azurermserviceplan" {
+#   name                = "JavaSpringSampleDeployServicePlan"
+#   location            = "East US"
+#   resource_group_name = "JavaSpringSampleDeploy-RG"
+#   os_type             = "Linux"
+#   sku_name            = "F1"
 
-  depends_on = [
-    azurerm_resource_group.JavaSpringSampleDeploy-RG
-  ]
-}
+#   depends_on = [
+#     azurerm_resource_group.JavaSpringSampleDeploy-RG
+#   ]
+# }
 
-resource "azurerm_linux_web_app" "azureappservice" {
-  name                = "JavaSpringSampleDeploy"
-  location            = "East US"
-  resource_group_name = "JavaSpringSampleDeploy-RG"
-  service_plan_id     = azurerm_service_plan.azurermserviceplan.id
+# resource "azurerm_linux_web_app" "azureappservice" {
+#   name                = "JavaSpringSampleDeploy"
+#   location            = "East US"
+#   resource_group_name = "JavaSpringSampleDeploy-RG"
+#   service_plan_id     = azurerm_service_plan.azurermserviceplan.id
 
-  site_config {
-    use_32_bit_worker = true
-    always_on = false
-    app_command_line = "java -jar /home/site/wwwroot/spring-azure-demo-0.0.1-SNAPSHOT.jar"
-  }
+#   site_config {
+#     use_32_bit_worker = true
+#     always_on = false
+#     app_command_line = "java -jar /home/site/wwwroot/spring-azure-demo-0.0.1-SNAPSHOT.jar"
+#   }
 
-  depends_on = [
-    azurerm_resource_group.JavaSpringSampleDeploy-RG
-  ]
+#   depends_on = [
+#     azurerm_resource_group.JavaSpringSampleDeploy-RG
+#   ]
 
-}
+# }
